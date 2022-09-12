8a64ccdc9c81316bf0667452299c280dca215e5b
Ryan Yoshimura <ryan.s.yoshimura@gmail.com>
2022-09-12 16:26:40 -0500

    posts controller complete, working on users-controller

--- src/main/resources/static/js/views/Register.js
+++ src/main/resources/static/js/views/Register.js
@@ -1,15 +1,17 @@
-import CreateView from "../createView.js"
 
+
+import CreateView from "../createView.js"
 export default function Register(props) {
     return `
     <!DOCTYPE html>
-        <html lang="Eng">
+        <html lang="ENG">
             <head>
                 <meta charset="UTF-8"/>
                 <title>Register</title>
             </head>
             <body>
                 <h1>Register</h1>
+        
                 <form id="register-form">
                     <label for="username">Username</label>
                     <input id="username" name="username" type="text"/>
@@ -23,29 +25,27 @@ export default function Register(props) {
         </html>
 `;
 }
-
 export function RegisterEvent(){
-    $("#register-btn").click(function(){
-
+    const registerButton = document.querySelector("#register-btn");
+    registerButton.addEventListener("click", function(event) {
+        const usernameField = document.querySelector("#username");
+        const emailField = document.querySelector("#email");
+        const passwordField = document.querySelector("#password");
         let newUser = {
-            username: $("#username").val(),
-            email: $("#email").val(),
-            password: $("#password").val()
+            userName: usernameField.value,
+            email: emailField.value,
+            password: passwordField.value
         }
-
-        console.log(newUser);
-
+        // console.log(newUser);
         let request = {
             method: "POST",
             headers: {"Content-Type": "application/json"},
             body: JSON.stringify(newUser)
         }
-
-        fetch("http://localhost:8080/api/users", request)
+        fetch(USER_API_BASE_URL + "/create", request)
             .then(response => {
                 console.log(response.status);
                 CreateView("/");
             })
-
     })
 }
