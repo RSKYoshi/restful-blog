1ba2fff1438758a601310d318f6f8bdccb43ac90
Ryan Yoshimura <ryan.s.yoshimura@gmail.com>
2022-09-12 12:07:00 -0500

    save post and edit functional

--- src/main/resources/static/js/views/PostIndex.js
+++ src/main/resources/static/js/views/PostIndex.js
@@ -1,15 +1,22 @@
 import CreateView from "../createView.js";
 
+let posts;
+
+
 export default function PostIndex(props) {
+    const postsHTML = generatePostsHTML(props.posts);
+    posts=props.posts;
+
     return `
         <header>
             <h1>Posts Page</h1>
         </header>
         <main>
               <h3>Lists of posts</h3>
-            <div>
-                ${props.posts.map(post => `<h3>${post.title}</h3>`).join('')}   
+              <div>
+                ${postsHTML}   
             </div>
+           
             <h3>Add a post</h3>
             <form>
                 <label for="title">Title</label><br>
@@ -17,20 +24,92 @@ export default function PostIndex(props) {
                 <br>
                 <label for="content">Content</label><br>
                 <textarea id="content" name="content" rows="10" cols="50" placeholder="Enter content"></textarea>
-                <button id="addPost" name="addPost">Add Post</button>
+                <button data-id="0" id="savePost" name="savePost">Save Post</button>
             </form>
         </main>
     `;
 }
 
+function generatePostsHTML(posts) {
+    let postsHTML = `
+        <table class="table">
+        <thead>
+        <tr>
+            <th scope="col">Title</th>
+            <th scope="col">Content</th>
+        </tr>
+        </thead>
+        <tbody>
+    `;
+    for (let i = 0; i < posts.length; i++) {
+        const post = posts[i];
+        postsHTML += `<tr>
+            <td>${post.title}</td>
+            <td>${post.content}</td>
+            <td><button data-id=${post.id} class="editPost">Edit</button></td>
+            <td><button data-id=${post.id} class="deletePost">Delete</button></td>
+            </tr>`;
+    }
+    postsHTML += `</tbody></table>`;
+    return postsHTML;
+}
+
 export function postSetup() {
-    addPostHandler();
-    // editPostHandler setup
+    // addPostHandler();
+    setupEditHandlers();
+    setupSaveHandler();
     // deletePostHandler setup
 }
 
+
+
+function setupEditHandlers() {
+    // target all delete buttons
+    const editButtons = document.querySelectorAll(".editPost");
+    // add click handler to all delete buttons
+    for (let i = 0; i < editButtons.length; i++) {
+        editButtons[i].addEventListener("click", function(event) {
+
+            // get the post id of the delete button
+            const postId = parseInt(this.getAttribute("data-id"));
+
+            loadPostIntoForm(postId);
+        });
+    }
+}
+
+function loadPostIntoForm(postId) {
+    // go find the post in the posts data that matches postId
+    const post = fetchPostById(postId);
+    if(!post) {
+        console.log("did not find post for id " + postId);
+        return;
+    }
+
+    // load the post data into the form
+    const titleField = document.querySelector("#title");
+    const contentField = document.querySelector("#content");
+    titleField.value = post.title;
+    contentField.value = post.content;
+
+    const saveButton = document.querySelector("#savePost");
+    saveButton.setAttribute("data-id", postId);
+}
+
+function fetchPostById(postId) {
+    for (let i = 0; i < posts.length; i++) {
+        if(posts[i].id === postId) {
+            return posts[i];
+        }
+
+    }
+    // didn't find it so return something falsy
+    return false;
+}
+
+
 function addPostHandler() {
-    const addButton = document.querySelector("#addPost");
+    const addButton = document.querySelector("#savePost");
     addButton.addEventListener("click", function(event) {
         const titleField = document.querySelector("#title");
         const contentField = document.querySelector("#content");
@@ -53,3 +132,45 @@ function addPostHandler() {
             })
     });
 }
+
+function setupSaveHandler(postId) {
+    const saveButton = document.querySelector("#savePost");
+    saveButton.addEventListener("click", function(event) {
+
+        // TODO: refactor later to a separate function for hygiene
+
+        // TODO: check the data-id for the save button
+        const postId = parseInt(this.getAttribute("data-id"));
+
+        // get the title and content for the new/updated post
+        const titleField = document.querySelector("#title");
+        const contentField = document.querySelector("#content");
+
+        // make the new/updated post object
+        const post = {
+            title: titleField.value,
+            content: contentField.value
+        }
+
+        // make the request
+        const request = {
+            method: "POST",
+            headers: {"Content-Type": "application/json"},
+            body: JSON.stringify(post)
+        }
+        let url = "http://localhost:8080/api/posts";
+
+        // if we are updating a post, change the request and the url
+        if(postId > 0) {
+            request.method = "PUT";
+            url += `/${postId}`;
+        }
+
+        fetch(url, request)
+            .then(function(response) {
+                // TODO: check the status code
+                CreateView("/posts");
+            })
+    });
+}
+
