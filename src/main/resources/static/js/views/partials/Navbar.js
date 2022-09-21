68cab4cc301c5abc41b4c988cad4011731a3c426
Ryan Yoshimura <ryan.s.yoshimura@gmail.com>
2022-09-20 19:03:14 -0500

    working on bootstrap navbar spacing and placement

--- src/main/resources/static/js/views/partials/Navbar.js
+++ src/main/resources/static/js/views/partials/Navbar.js
@@ -2,26 +2,26 @@ import {isLoggedIn} from "../../auth.js";
 
 export default function Navbar(props) {
 
-    let navbar = `<nav>
-        <a href="/" data-link>Home<a/>
-        <a href="/register" data-link>Register</a>
-        <a href="/posts" data-link>Posts</a>
+    let navbar = `<nav class="d-flex bd-highlight mb-3 mt-1">
+            <a href="/" data-link class="nav-item text-white bg-success badge p-2 bd-highlight">Home<a/>
+            <a href="/register" data-link class="nav-item text-white bg-success badge p-2 bd-highlight">Register</a>
+            <a href="/posts" data-link class="nav-item text-white bg-success badge p-2 bd-highlight">Posts</a>
 
 
     `;
     if(isLoggedIn()){
         navbar = `
-            <a href="/" data-link>Home<a/>
-            <a href="/about" data-link>About<a/>
-            <a href="/me" data-link>User</a>
-            <a href="/posts" data-link>Posts</a>
-            <a href="/logout" data-link>Logout</a>
+            <a href="/" data-link class="text-white bg-success badge me-auto p-2 bd-highlight list-group-item">Home<a/>
+            <a href="/about" data-link class="text-white bg-success badge me-auto p-2 bd-highlight list-group-item">About<a/>
+            <a href="/me" data-link class="text-white bg-success badge me-auto p-2 bd-highlight list-group-item">User</a>
+            <a href="/posts" data-link class="text-white bg-success badge me-auto p-2 bd-highlight list-group-item">Posts</a>
+            <a href="/logout" data-link class="text-white bg-success badge me-auto p-2 bd-highlight list-group-item">Logout</a>
 
 
         `;
     } else {
         navbar += `
-        <li class="list-group-item"><nav class="bg-success badge ml-auto w-25"><a href="/login" data-link class="text-white">Login<a/></nav></li>
+        <a href="/login" data-link class="nav-item text-white bg-success badge p-2 bd-highlight">Login<a/>
     `;
     }
     navbar += `</nav>`;
